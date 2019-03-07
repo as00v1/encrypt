1f716ef6a7e8c75854ad676a6b73de5578c2cc62
jiayong <36148963@qq.com>
2019-03-07 14:08:24 +0800

    增加了校验，完善了错误编码

---
+++ encrypt-utils/src/main/java/com/qiaohx/encryptutils/util/BaseDataResponse.java
@@ -0,0 +1,26 @@
+package com.qiaohx.encryptutils.util;
+
+/**
+ * 通用的数据包
+ */
+public class BaseDataResponse {
+
+    private int code;
+    private String errMsg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+}
