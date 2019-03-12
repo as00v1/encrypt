5bcaf6ccd82a1e5a26e64f258f406c431c2f1622
jiayong <36148963@qq.com>
2019-03-12 09:20:12 +0800

    小修改

--- encrypt-api/src/main/java/com/qiaohx/encryptapi/SwaggerConfig.java
+++ encrypt-api/src/main/java/com/qiaohx/encryptapi/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.qiaohx.encryptapi;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -13,6 +14,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 /**
  * 自动生成API文档的配置
  */
+@Configuration
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
